da532a423837a42592ffb0c4b4e0dfd380b24f61
FarazBhuiyan <faraz.bhuiyan@gmail.com>
2022-05-18 23:51:33 -0500

    revamped main page

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+/*!
+
+Holder - client side image placeholders
+Version 2.8.0+7srgw
+© 2015 Ivan Malopinsky - http://imsky.co
+
+Site:     http://holderjs.com
+Issues:   https://github.com/imsky/holder/issues
+License:  MIT
+
+Custom JS Page
+
+!*/
